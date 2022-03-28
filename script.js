943ef06026422a2f444c303b933845d40bcaf75b
Justas <justas.pnt@gmail.com>
2022-03-28 22:42:06 +0300

    added reset button

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 "use strict";
 
 const gridContainer = document.querySelector("#grid-container");
+const resetBtn = document.querySelector("#resetBtn");
 
+/* Sets grid items number and creates divs */
 const setGridItems = function (grids) {
   gridContainer.style.gridTemplateRows = `repeat(${grids}, 1fr)`;
   gridContainer.style.gridTemplateColumns = `repeat(${grids}, 1fr)`;
@@ -11,20 +13,42 @@ const setGridItems = function (grids) {
   }
 };
 
-setGridItems(16);
-const gridItems = gridContainer.querySelectorAll("div");
+setGridItems(12);
+let gridItems = gridContainer.querySelectorAll("div");
 
+/* creates random number between min and max numbers included. use from 0 to 255 */
 const randomInt = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-const paint = function (e) {
+/* painting functions */
+const paintRandom = function (e) {
   e.target.style.backgroundColor = `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(
     0,
     255
   )})`;
 };
 
+const reset = function () {
+  gridItems.forEach((item) => {
+    item.remove();
+  });
+
+  let gridsNumb = Number(window.prompt("Pad size(1-100", 12));
+
+  if (!Number.isInteger(gridsNumb) || gridsNumb < 1 || gridsNumb > 100) {
+    gridsNumb = 12;
+  }
+
+  setGridItems(gridsNumb);
+  gridItems = gridContainer.querySelectorAll("div");
+  gridItems.forEach((item) => {
+    item.addEventListener("mouseover", paintRandom);
+  });
+};
+
 gridItems.forEach((item) => {
-  item.addEventListener("mouseover", paint);
+  item.addEventListener("mouseover", paintRandom);
 });
+
+resetBtn.addEventListener("click", reset);
